c8774e4e3b141aa5e293959855dc50287517d49a
anu-ban <wwr350i@gmail.com>
2023-03-07 06:09:49 +0000

    Contains Commited

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,14 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
-        return false;
+        boolean isOn=false;
+
+        for(int i=0; i<arr.length;i++){
+            if(target==arr[i]){
+                isOn= true;
+            }
+        }
+        return isOn;
+        
     }
 }
